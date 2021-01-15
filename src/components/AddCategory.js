cd70eb6241019e87fcea299b3f36abe789a37960
Victor Corsino <vcorsinoa@gmail.com>
2021-01-15 16:11:19 -0400

    First commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ( {setCategories} ) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const hadleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //console.log('Submit hecho')
+        if ( inputValue.trim().length > 2) {
+            setCategories( cats => [ inputValue,...cats, ] )
+            setInputValue('');
+        }
+
+        
+
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input 
+                type="text"
+                value={ inputValue }
+                onChange={ hadleInputChange }
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
